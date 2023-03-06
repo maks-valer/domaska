ebfd6d71094bbb8ad7fd3bb806f0aff13bbbe130
maks-valer <maksimka-korotkov@yandex.ru>
2023-03-06 16:12:50 +0300

    new fail

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+// int[] L = new int[100]; 
+while(current <= num)
+{
+    // L.Append[Math.Pow(current, 2)];
+    Console.WriteLine(Math.Pow(current, 2));
+    // Console.WriteLine(L);
+    current ++;
+}
